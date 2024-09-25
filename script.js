d4d2132e7592d0adeb89ba08e56a1c831e8d2efc
Gabriel Couto <aluno@fiap.com.br>
2024-09-25 09:52:34 -0300

    Exercicio 2

--- script.js
+++ script.js
@@ -11,3 +11,10 @@ console.log(`Variável Vazia: ${var_vazia}`);
 var variavel_com_valor = "checkpoint 1";
 console.log(`Variável com valor: ${variavel_com_valor}`);
 // declaração da variável com valor
+
+// ex 2
+
+var string1 = "132.842";
+let conversao_string1 = parseFloat(string1);
+console.log(`String convertida para float: ${conversao_string1}`)
+console.log(`Tipo da variável convertida: ${typeof(conversao_string1)}`);
