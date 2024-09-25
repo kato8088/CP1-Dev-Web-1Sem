5bdb0afbf6ba5291c3b257708022973d862892a4
Gabriel Kato <aluno@fiap.com.br>
2024-09-25 09:51:41 -0300

    Exercicio 1

---
+++ script.js
@@ -0,0 +1,13 @@
+// ex 1
+
+let var_indefinida;
+console.log(`Variável Indefinida: ${var_indefinida}`);
+// declaração da variável indefinida
+
+let var_vazia = null;
+console.log(`Variável Vazia: ${var_vazia}`);
+// declaração da variável vazia
+
+var variavel_com_valor = "checkpoint 1";
+console.log(`Variável com valor: ${variavel_com_valor}`);
+// declaração da variável com valor
