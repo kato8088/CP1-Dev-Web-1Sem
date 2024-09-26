07700631df1c0f2a52703c92d12ae1b933d2bc8a
Francisco Vargas <aluno@fiap.com.br>
2024-09-26 13:44:45 -0300

    Exercicio 3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// ex 1
+// ex 1:
 
 let var_indefinida;
 console.log(`Variável Indefinida: ${var_indefinida}`);
@@ -12,9 +12,22 @@ var variavel_com_valor = "checkpoint 1";
 console.log(`Variável com valor: ${variavel_com_valor}`);
 // declaração da variável com valor
 
-// ex 2
+// ex 2:
 
 var string1 = "132.842";
 let conversao_string1 = parseFloat(string1);
 console.log(`String convertida para float: ${conversao_string1}`)
-console.log(`Tipo da variável convertida: ${typeof(conversao_string1)}`);
+console.log(`Tipo da variável convertida: ${typeof (conversao_string1)}`);
+
+// ex 3:
+// Declarando variavel de tipo inteiro
+let numeroInteiro = 10;
+console.log("Valor original:", numeroInteiro);
+
+// Convertendo para float
+let numeroFloat = parseFloat(numeroInteiro);
+console.log("Valor convertido:", numeroFloat);
+
+// Verificando o tipo da variável após a conversão
+let verTipo = typeof numeroInteiro
+console.log(verTipo);
